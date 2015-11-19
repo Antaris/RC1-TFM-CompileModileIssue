e29ab8946621739551b0ea0c124fc2e4a15456f9
Matthew Abbott <me@fidelitydesign.net>
2015-11-19 11:56:40 +0000

    Initial commit

---
+++ src/ConsoleApp1/compiler/preprocess/RunExampleCompileModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.compiler.preprocess
+{
+    public class RunExampleCompileModule : ClassLibrary1.ExampleCompileModule
+    {
+    }
+}
